184abbf37f8d191d36225b4d8ab248ee9e43eb7a
pbalduino <pbalduino+github@gmail.com>
2011-07-07 15:43:03 -0300

    Now the Treetop result is converted to a list of lists, making easy to eval the whole expression

--- lib/lucio/grammar.rb
+++ lib/lucio/grammar.rb
@@ -1,10 +1,22 @@
 module Lucio
   class Grammar < Treetop::Runtime::SyntaxNode
     def eval
-      make_tree(make_list(elements))
+      tree = make_tree(elements)
+      p case
+        when tree.empty?
+          nil
+        when tree.size == 1
+          Kernel::eval(tree[0])
+        else
+          tree
+      end
     end
 
     private
+      def make_tree(el)
+        Kernel::eval(make_list(el).inject('') {|x, y| x += "'#{y}',"}.strip.chop.gsub(/'\(',/, '[').gsub(/[,]?'\)'/, ']'))
+      end
+
       def make_list(el, list = [])
         el.each do |e|
           unless e.empty? || e.text_value.strip.empty?
--- spec/eval_spec.rb
+++ spec/eval_spec.rb
@@ -3,9 +3,11 @@ require 'lucio'
 
 describe Lucio do
   context 'eval' do
-    it 'fails' do
-      # Lucio.eval('()').should == nil
-      Lucio.eval('( )')
+    it 'should return a value' do
+      Lucio.eval('()').should == nil
+      Lucio.eval('( )').should == nil
+      Lucio.eval('(  )').should == nil
+      Lucio.eval('(1)').should == 1
       Lucio.eval('(+ 1 2)')
       Lucio.eval('(* 2 (+ 3 4))')
     end
